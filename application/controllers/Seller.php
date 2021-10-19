0c81085de0ec7c0bc326f6343ceee50896deeb2a
BrenNevil <85412702+BrenNevil@users.noreply.github.com>
2021-10-19 02:16:03 -0500

    Update Seller.php

--- application/controllers/Seller.php
+++ application/controllers/Seller.php
@@ -5,7 +5,7 @@ class seller extends CI_Controller {
 
   public function __construct() {
     parent::__construct();    
-    $this->load->model('seller_model');          
+    $this->load->model('Seller_model');          
   }
 
 

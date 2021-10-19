8539e1c557c26135a21430085ef106704a783c81
BrenNevil <85412702+BrenNevil@users.noreply.github.com>
2021-10-19 01:36:17 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://proyect-payment.herokuapp.com/public';
+$config['base_url'] = 'https://proyect-payment.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------

aca9d189b422bae3f2f028ebc9cc03f092814600
BrenNevil <85412702+BrenNevil@users.noreply.github.com>
2021-10-19 11:21:37 -0500

    Revert "Delete application/models directory"
    This reverts commit 346a6b937590fdfbc2672e07df5eeba6def3bbfd.

---
+++ application/models/product_model.php
@@ -0,0 +1,40 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Product_model extends CI_Model {   
+		function __construct()
+		{
+					parent::__construct();
+		}             
+					 
+
+    public function get_all_products(){
+
+     return $this->db->get("products")->result();    
+
+    }
+
+    public function get_products_order($id){
+        $this->db->from("products_order");
+        $this->db->where("id_order", $id);
+	    $query = $this->db->get()->result();
+	    foreach ($query as $row) {
+			
+		 $this->db->from("products");
+		 $this->db->where("id_product", $row->id_product); 
+		 $query_prod = $this->db->get();
+		 $row_prod = $query_prod->row();	 
+			
+	     $row->name  = $row_prod -> name;
+	     $row->price = $row_prod -> price;
+	 }
+	return $query;
+     
+    }
+  
+
+
+
+}
+?>
